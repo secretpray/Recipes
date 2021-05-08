0bb93a654e227700e8df2d987df5f70ba2f5bb42
secretpray <Renovatio100>
2021-05-08 20:06:42 +0300

    Added Breadcrumbs, Styling Main/Show/Category page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,34 @@
 module ApplicationHelper
   
+  # Navigation menu activa
   def is_active(controller, action)       
     params[:action] == action && params[:controller] == controller ? "active" : nil        
   end
 
+  # Form helper
   def link_form(object)
     object.persisted? ? 'Update' : 'Create'
   end
 
+  # Theme select
   def background_select       
     if cookies[:theme] == 'light'
       'navbar-light bg-light'
     else
       'navbar-light bg-warning'
     end
+  end
+
+  #  Breadcrumbs
+  def build_breadcrumbs(category)
+    result = []
+    category.ancestors.each do |ancestor|
+      result << content_tag(:li, class: 'breadcrumb-item') do
+        link_to(ancestor.name, ancestor, class: 'text-decoration-none')
+      end
+    end
+    result << content_tag(:li, category.name, class: 'breadcrumb-item active ')
+    result.join('').html_safe
   end
 end
 
