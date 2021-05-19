3563796f5ae7ae6820926c7701f4f8318adae37c
secretpray <Renovatio100>
2021-05-19 22:35:23 +0300

    heroku error -> , raise: false

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,5 @@
 class ErrorsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, raise: false
 
   def not_found
     render status: 404
