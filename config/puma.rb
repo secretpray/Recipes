f6aca28dae66fd07868651cd55b1c1c98000162b
secretpray <Renovatio100>
2021-05-20 12:56:43 +0300

    Server Puma errors fixed

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -15,7 +16,12 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
+# def port(port, host=nil)
+#   host ||= default_host
+#   bind "tcp://#{host}:#{port}"
+# end
 port ENV.fetch("PORT") { 3000 }
+# port ENV['PORT'] || 3000
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -37,7 +43,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
+rackup      DefaultRackup
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
