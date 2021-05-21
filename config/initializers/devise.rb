cd661131f225fbfb90b3699735ebf62c27ae2376
secretpray <Renovatio100>
2021-05-21 07:36:39 +0300

    Fixed bug with Facebook omniauth login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,13 +276,13 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   # Rails.application.credentials.dig(:facebook, :facebook_client_secret), scope: [:email]
   config.omniauth :facebook, Rails.application.credentials.dig(:facebook, :facebook_client_id),
-  Rails.application.credentials.dig(:facebook, :facebook_client_secret)
+  Rails.application.credentials.dig(:facebook, :facebook_client_secret), scope: 'email, public_profile', info_fields: 'first_name, last_name'
   config.omniauth :github, Rails.application.credentials.dig(:github, :github_client_id),
   Rails.application.credentials.dig(:github, :github_client_secret), scope: "user, public_repo, gist"
   config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :google_client_id),
-  Rails.application.credentials.dig(:google, :google_client_secret), scope: 'userinfo.email, userinfo.profile' 
+  Rails.application.credentials.dig(:google, :google_client_secret), scope: 'userinfo.email, userinfo.profile'
   # , userinfo.first_name, userinfo.last_name, userinfo.image
-  
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
