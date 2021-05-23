2f9f9e6580f0a1592ccccbdd6eb144130b3092ff
secretpray <Renovatio100>
2021-05-23 09:03:04 +0300

    Added Terms, Privacy, Limits

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'errors/not_found'
-  get 'errors/internal_server_error'
+  # get 'errors/not_found'
+  # get 'errors/internal_server_error'
   get 'favorites/update'
   get 'recipes/favorites'
-  
+
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
   resources :users, only: [:show] do
-    member do 
+    member do
       get 'recipes', to: 'users#recipes'
       patch 'change_status', to: 'users#change_status' # patch :change_status
     end
@@ -16,17 +16,20 @@ Rails.application.routes.draw do
   resources :tags, only: [:show]
   resources :recipes do
     resources 'reviews', only: [:index, :new, :create]
-    resources 'comments', only: [:create, :destroy] do 
+    resources 'comments', only: [:create, :destroy] do
       member do
         patch "upvote", to: "comments#upvote"
         patch "downvote", to: "comments#downvote"
       end
     end
   end
-  
+
+  get '/privacy', to: 'dashboard#privacy'
+  get '/terms', to: 'dashboard#terms'
+  get '/limits', to: 'dashboard#limits'
   match "/404", via: :all, to: "errors#not_found"
   match "/500", via: :all, to: "errors#internal_server_error"
-  
+
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
