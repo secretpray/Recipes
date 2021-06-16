d279525c9278bc0ec1f53c4050639a0558c438ee
secretpray <Renovatio100>
2021-06-16 11:36:56 +0300

    Some JS fixed

--- app/javascript/packs/cloud3d/init.js
+++ app/javascript/packs/cloud3d/init.js
@@ -1,15 +1,23 @@
 document.addEventListener("turbolinks:load", function() {
-  try {
-    // TagCanvas.Start('myCanvas');
-    TagCanvas.Start('myCanvas','tags',{
-            textColour: 'rgb(10, 88, 202)',
-            outlineColour: 'rgba(60, 85, 200, 0.26)',
-            reverse: true,
-            depth: 0.9,
-            maxSpeed: 0.05
-          });
-  } catch(e) {
-    // something went wrong, hide the canvas container
-    document.getElementById('myCanvasContainer').style.display = 'none';
+  const controllerName = document.body.dataset.controllerName
+  const actionName = document.body.dataset.actionName
+
+  if (controllerName == 'dashboard' && actionName == 'index') {
+    try {
+      // TagCanvas.Start('myCanvas');
+      TagCanvas.Start('myCanvas','tags',{
+              textColour: 'rgb(10, 88, 202)',
+              outlineColour: 'rgba(60, 85, 200, 0.26)',
+              reverse: true,
+              depth: 0.9,
+              maxSpeed: 0.05
+            });
+    } catch(e) {
+      // something went wrong, hide the canvas container
+      var containerCanvas = document.getElementById('myCanvasContainer')
+      if (containerCanvas) {
+        containerCanvas.style.display = 'none';
+      }
+    }
   }
 });
