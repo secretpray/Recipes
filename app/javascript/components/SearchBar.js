2e808787797ba94c6b8463618ab5f12446aad98d
secretpray <Renovatio100>
2021-10-14 21:24:26 +0300

    Refactoring to func React components

--- app/javascript/components/SearchBar.js
+++ app/javascript/components/SearchBar.js
@@ -1,34 +1,28 @@
 import React from "react"
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const SearchBar = (props) => {
+  return (
+    <form action="/autosearch" acceptCharset="UTF-8" method="get">
+      <input name="utf8" type="hidden" value="√" />
+      <button name="button" type="submit">
+        <i className="fa fa-search" id='searchbar-icon'></i>
+      </button>
+      <input
+        onFocus={() => props.showDropdown()}
+        onBlur={() => props.hideDropdown()}
+        value={props.term}
+        onChange={(event) => {handleInputChange(event.target.value)}}
+        placeholder="Search"
+        autoComplete="off"
+        type="search"
+        name="search[q]"
+        className='ps-5 py-1 nav-link'
+        id="search_q" />
+    </form>
+  )
 
-  render () {
-    return (
-      <form action="/autosearch" acceptCharset="UTF-8" method="get">
-        <input name="utf8" type="hidden" value="√" />
-        <button name="button" type="submit">
-          <i className="fa fa-search" id='searchbar-icon'></i>
-        </button>
-        <input
-          onFocus={() => this.props.showDropdown()}
-          onBlur={() => this.props.hideDropdown()}
-          value={this.props.term}
-          onChange={(event) => {this.handleInputChange(event.target.value)}}
-          placeholder="Search"
-          autoComplete="off"
-          type="search"
-          name="search[q]"
-          className='ps-5 py-1 nav-link'
-          id="search_q" />
-      </form>
-    );
-  }
-
-  handleInputChange(term) {
-    this.props.onSearchTermChange(term);
+  function handleInputChange(term) {
+    props.onSearchTermChange(term)
   }
 }
 
--- app/javascript/components/SearchUserListItem.js
+++ app/javascript/components/SearchUserListItem.js
@@ -1,15 +1,14 @@
 import React from "react"
 
-class SearchUserListItem extends React.Component {
-  render() {
-    return (
-      <li className='list-item'>
-        <a className='text-decoration-none list-item-link' href={this.props.user.user_url}>
-          <img width="35" className="avatar-image" src={this.props.user.user_avatar_url} />
-          <span className='list-item-span' dangerouslySetInnerHTML={{ __html: this.props.user.username }} />
-        </a>
-      </li>
-    );
-  }
+const SearchUserListItem = (props) => {
+  return (
+    <li className='list-item'>
+      <a className='text-decoration-none list-item-link' href={props.user.user_url}>
+        <img width="35" className="avatar-image" src={props.user.user_avatar_url} />
+        <span className='list-item-span' dangerouslySetInnerHTML={{ __html: props.user.username }} />
+      </a>
+    </li>
+  )
 }
+
 export default SearchUserListItem
