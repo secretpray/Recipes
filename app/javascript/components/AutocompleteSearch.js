f5bfaf0e6c5d69c542bbd2faaa172a4ff8e62dc9
secretpray <Renovatio100>
2021-10-17 23:16:21 +0300

    Refactoring sync/await, add Loader

--- app/javascript/components/AutocompleteSearch.js
+++ app/javascript/components/AutocompleteSearch.js
@@ -1,11 +1,21 @@
 import React, {useState, useEffect, useReducer} from 'react'
 import SearchBar from "./SearchBar"
 import SearchResultsList from "./SearchResultsList"
+import loader from '../../assets/images/loader/spinner6.gif'
+
+
+const baseURL = 'react_autosearch?term='
+const configFetch = {
+  method: 'GET',
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+}
 
 export const ACTIONS = {
   CHANGE_TERM: 'change-term',
   FETCHED: 'fetched',
-  LOADING: 'isloading',
   PREVENT_HIDE_DROPDOWN: 'prevent-hide-dropdown',
   SHOW_DROPDOWN: 'show-dropdown'
 }
@@ -13,12 +23,19 @@ export const ACTIONS = {
 const initialState = {
   preventHideDropdown: false,
   showDropdown: true,
-  isloading: false,
   term: '',
   recipes: [],
   users: [],
   tags: []
-};
+}
+
+const styleLoader = { width: '25px',
+                      objectFit: 'scale-down',
+                      position: 'absolute',
+                      opacity:'0.4',
+                      align: 'middle',
+                      left: '64%',
+                      margin: '5px'}
 
 const getAttributes = (response) => {
   const { data } = JSON.parse(response || 'false')
@@ -32,8 +49,6 @@ const reducer = (state, action) => {
     case ACTIONS.CHANGE_TERM:
       return { ...state,
               term: action.payload }
-    case ACTIONS.LOADING:
-      return { ...state, isloading: action.payload }
     case ACTIONS.FETCHED:
       const { recipes, users, tags } = action.payload
       return { ...state,
@@ -54,48 +69,51 @@ const reducer = (state, action) => {
         return state
       }
     default:
-      throw new Error(`${action.type} - not found!`)
+      throw new Error(`${action.type} - not found!`);
   }
 }
 
-const AutocompleteSearch = (props) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const search = (term) => {
-    fetch(`react_autosearch?term=${term}`)
-      .then(response => response.json())
-      .then(data => {
-        const { recipes, users, tags } = data
-        dispatch({type: ACTIONS.FETCHED, payload: { recipes, users, tags }})
-      })
-  }
+const AutocompleteSearch = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
     let term = state.term
-    const debounceSearch = setTimeout(() => {
-      if(term?.length > 1) {
-        search(term)
-      }
-    }, 300)
-    return () => {
-      clearTimeout(debounceSearch)
+
+    const search = async (term) => {
+      setIsLoading(true)
+      const response = await fetch(baseURL + `${term}`, configFetch)
+      const { recipes, users, tags } = await response.json()
+      dispatch({type: ACTIONS.FETCHED, payload: { recipes, users, tags}})
+      setIsLoading(false)
     }
+
+    const debounceSearch = setTimeout(() => {
+      if(term?.length > 1) { search(term) }}, 300)
+    return () => {clearTimeout(debounceSearch)}
   }, [state.term])
 
   const renderSearchResults = () => {
+    // {isError && <div>Something went wrong ...</div>}
+    if(isLoading) {
+      return (
+        <img src={loader} style={styleLoader} alt="loading..." />
+      )
+    } else {
+      const notContentFound = !state.recipes?.length && !state.users?.length && !state.tags?.length
+      if(!state.showDropdown || notContentFound) { return }
 
-    const anyContentPresent = !state.recipes?.length && !state.users?.length && !state.tags?.length
-    if(!state.showDropdown || anyContentPresent) { return }
-
-    return (
-      <SearchResultsList
-        term={state.term}
-        recipes={state.recipes}
-        users={state.users}
-        tags={state.tags}
-        dispatch={dispatch}
-      />
-    )
+      return (
+        <SearchResultsList
+          term={state.term}
+          recipes={state.recipes}
+          users={state.users}
+          tags={state.tags}
+          dispatch={dispatch}
+        />
+      )
+    }
   }
 
   return (
